0fd018a8acc40019d999019537ad6c6a1b78d7a4
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2026-01-21 03:01:20 +0000

    💄 ui(_layout):Se modifica el ui -Se modifica el ui en maneras diversas{     - Se oculta la barra de navegación de index.     - Se crea los link necesarios para la navegación de la pagina }

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import '@/global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#1F2937' },
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="games/dice" />
+      </Stack>
+    </>
+  );
 }
